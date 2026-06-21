b5efc394ec67b5d55f2bde8bf9edec71d8bce85d
kaydee249 <kasan249@yahoo.com>
2026-06-20 22:33:05 -0400

    test break

--- app/src/test/java/guru/elevatehub/CalculatorTest.java
+++ app/src/test/java/guru/elevatehub/CalculatorTest.java
@@ -14,7 +14,7 @@ class CalculatorTest {
 
     @Test
     void addsTwoNumbers() {
-        assertEquals(5, calculator.add(2, 3));
+        assertEquals(6, calculator.add(2, 3));
         assertEquals(0, calculator.add(-1, 1));
     }
 
