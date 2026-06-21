e1e2fee9b6197656a1b393ef6b37a2ea92ee3488
kaydee249 <kasan249@yahoo.com>
2026-06-20 22:55:57 -0400

    fix changes

--- app/src/main/java/guru/elevatehub/Calculator.java
+++ app/src/main/java/guru/elevatehub/Calculator.java
@@ -7,7 +7,7 @@ package guru.elevatehub;
 public class Calculator {
 
     public int add(int a, int b) {
-        return a + b;
+        return a + b
     }
 
     public int subtract(int a, int b) {
